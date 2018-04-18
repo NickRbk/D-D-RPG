97427056140331286ad569104d1d57d816b29c94
Nick Rybak <nrybak94@gmail.com>
2018-04-18 20:41:06 +0300

    task#9: move methods from mazeHarbour branch

--- src/main/java/cursor/rybak/game/Game.java
+++ src/main/java/cursor/rybak/game/Game.java
@@ -1,8 +1,14 @@
 package cursor.rybak.game;
 
+import cursor.rybak.model.maze.AbstractMaze;
+import cursor.rybak.model.maze.Location;
+import cursor.rybak.model.maze.impl.Maze;
+import cursor.rybak.model.maze.map.MazeMap;
 import cursor.rybak.model.team.Team;
 import cursor.rybak.view.GeneralMessage;
 import cursor.rybak.view.TeamMessage;
+
+import java.util.Map;
 
 public class Game {
     public static void start() {
@@ -12,5 +18,27 @@ public class Game {
 
         Team team = new Team(teamName);
         TeamMessage.printTeamInfo(team);
+    }
+
+    private static void throwTeamOnLocation(AbstractMaze maze, Team team) {
+        Map<String, Location> mazeMap = MazeMap.getMazeMap(maze.getMaze());
+
+        while(true) {
+            int positionX = (int)Math.round( Math.random() * maze.getWidth());
+            int positionY = (int)Math.round( Math.random() * maze.getHeight());
+            String key = String.format("%d|%d", positionX, positionY);
+
+            if (mazeMap.get(key).isPath()) {
+                mazeMap.get(key).setTeam(team);
+                maze.getMaze()[positionX][positionY] = 2;
+                return;
+            }
+        }
+    }
+
+    private static void enterToMaze(Team team, String gameMode) {
+        AbstractMaze maze = new Maze(Mode.valueOf(gameMode).getWidth(), Mode.valueOf(gameMode).getHeight());
+        throwTeamOnLocation(maze, team);
+        maze.print();
     }
 }
